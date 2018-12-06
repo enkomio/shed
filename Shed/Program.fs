3ecd66923287e07d121e18207ae78089f38d30f5
Antonio Parata <aparata@gmail.com>
2018-12-06 20:49:53 +0100

    Shed debugger

--- Shed/Program.fs
+++ Shed/Program.fs
@@ -7,11 +7,13 @@ open System.IO
 open System.Diagnostics
 open Argu
 open ES.Shed
+open System.Security.Principal
 
 module Program =
     type CLIArguments =
         | Dump_Heap
-        | Dump_Modules        
+        | Dump_Modules    
+        | Extract_Expression
         | Pid of pid:Int32
         | Exe of file:String
         | Timeout of timeout:Int32
@@ -23,13 +25,14 @@ module Program =
                 match s with
                 | Dump_Modules _ -> "dump all the .NET modules from the running program."
                 | Dump_Heap -> "dump all objects found in the heap." 
+                | Extract_Expression -> "evaluate an expression from the specified process/pid."
                 | Pid _ -> "the id of the process to inspect." 
                 | Exe _ -> "a filename to execute and inspect."
-                | Timeout _ -> "wait the given amount of milliseconds before to inspect the process. This is only valid if an exe is specified."
+                | Timeout _ -> "wait the given amount of milliseconds before to inspect or debug the process."
                 | Verbose _ -> "print verbose messages."
                 | Version _ -> "print the Shed version."
 
-    let printBanner() =
+    let private printBanner() =
         Console.ForegroundColor <- ConsoleColor.Cyan        
         let banner = "-=[ Shed .NET program inspector ]=-"
         let year = if DateTime.Now.Year = 2017 then "2017" else String.Format("2017-{0}", DateTime.Now.Year)
@@ -38,49 +41,75 @@ module Program =
         Console.WriteLine(copy)
         Console.ResetColor()
 
-    let printUsage(body: String) =
+    let private printUsage(body: String) =
         Console.WriteLine(body)
 
-    let printError(errorMsg: String) =
+    let private printError(errorMsg: String) =
         Console.ForegroundColor <- ConsoleColor.Red
         Console.WriteLine(errorMsg)
         Console.ResetColor()
 
-    let printVersion() =
+    let private printVersion() =
         let version = FileVersionInfo.GetVersionInfo(Assembly.GetExecutingAssembly().Location).ProductVersion
         Console.WriteLine("Shed version: {0}", version)
+
+    let private isAdmin() =
+        (new WindowsPrincipal(WindowsIdentity.GetCurrent())).IsInRole(WindowsBuiltInRole.Administrator)
         
-    let runFramework(pid: Int32, results: ParseResults<CLIArguments>) =
+    let runFramework(shed: ShedFramework, results: ParseResults<CLIArguments>) =
+        let shedApplication = new ShedApplication(shed)
+        
+        let timeout = results.GetResult(<@ Timeout @>, 1000)
         let dumpModules = results.Contains(<@ Dump_Modules @>)
         let dumpHeap = results.Contains(<@ Dump_Heap @>)
+        let extractExpression = results.Contains(<@ Extract_Expression @>)        
+        let dumpAll = not dumpModules && not dumpHeap && not extractExpression
+
+        // as first operation run the extraction with the debugger.
+        // It is very important that the extraction is the first command, 
+        // otherwise the clrmd framework will hang the debugger
+        if extractExpression then
+            shedApplication.Extract()
+        else
+            // run the program to run for timeout milliseconds
+            // this will aloows the debugger to catch usefull information
+            shedApplication.Run(timeout)
+
+        // run the remaining option in no particular order
+        [            
+            (dumpHeap || dumpAll, shedApplication.DumpHeap)
+            (dumpModules || dumpAll, shedApplication.DumpModules)                        
+        ] 
+        |> List.filter fst
+        |> List.map snd
+        |> List.iter(fun callback -> callback())
+
+        // finally get the report
+        shedApplication.GenerateReport()
+
+    let createShedFramework(results: ParseResults<CLIArguments>) =
         let verbose = results.Contains(<@ Verbose @>)
-        let timeout = results.TryGetResult(<@ Timeout @>)
-        let dumpAll = not(dumpModules || dumpHeap)
-        
         let messageBus = new MessageBus()
         use shed = new ShedFramework(messageBus)
 
         if verbose then
             messageBus.Dispatch(new SetLoggingLevelCommand(LogLevel.Trace))
 
-        if timeout.IsSome then 
-            Thread.Sleep(timeout.Value)
-
-        if shed.Attach(pid) then
-            let shedApplication = new ShedApplication(shed)
-            [
-                (dumpModules || dumpAll, shedApplication.DumpModules)
-                (dumpHeap || dumpAll, shedApplication.DumpHeap)
-            ] 
-            |> List.filter fst
-            |> List.map snd
-            |> List.iter(fun callback -> callback())
-
-            // finally get the report
-            shedApplication.GenerateReport()
+        if not(isAdmin()) then
+            messageBus.Dispatch(new LogMessageEvent("You are not running Shed as Administrator, this may imply some limitation.", LogLevel.Warning))
             
-        else
-            printError("Unable to attach to the process id: " + pid.ToString())
+        shed
+
+    let runFrameworkWithPid(pid: Int32, results: ParseResults<CLIArguments>) = 
+        use shed = createShedFramework(results)
+        if shed.Attach(pid) 
+        then runFramework(shed, results)            
+        else printError("Unable to attach to the process id: " + pid.ToString())
+
+    let runFrameworkWithExe(program: String, results: ParseResults<CLIArguments>) =
+        use shed = createShedFramework(results)
+        shed.CreateProcess(program) 
+        runFramework(shed, results)
                 
     [<EntryPoint>]
     let main argv = 
@@ -103,21 +132,16 @@ module Program =
                     printUsage(parser.PrintUsage())   
                     1
                 | Some pid, None ->
-                    runFramework(pid, results)
+                    runFrameworkWithPid(pid, results)
                     0
                 | None, Some file ->
                     let fullPath = Path.GetFullPath(file)
-                    if not <| File.Exists(fullPath) then
-                        printError(String.Format("File '{0}' not found", fullPath))
-                        1
+                    if File.Exists(fullPath) then
+                        runFrameworkWithExe(fullPath, results)
+                        0                         
                     else
-                        let procStartInfo = new ProcessStartInfo(fullPath, UseShellExecute = true)
-                        let proc = Process.Start(procStartInfo)
-                                                
-                        runFramework(proc.Id, results)   
-                        if not proc.HasExited then
-                            proc.Kill()                     
-                        0                    
+                        printError(String.Format("File '{0}' not found", fullPath))
+                        1                 
         with 
             | :? ArguParseException ->
                 printUsage(parser.PrintUsage())   
--- Shed/ShedApplication.fs
+++ Shed/ShedApplication.fs
@@ -12,6 +12,13 @@ type ShedApplication(shed: ShedFramework) =
         let command = new DumpHeapCommand()
         shed.Run(command)
 
+    member this.Extract() =
+        let command = new ExtractCommand()
+        shed.Run(command)
+
+    member this.Run(milliseconds: Int32) =
+        shed.Go(milliseconds)
+
     member this.GenerateReport() =
         let command = new GenerateReportCommand()
         shed.Run(command)
